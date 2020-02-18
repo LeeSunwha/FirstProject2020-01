7c332448b4700708ed2bb9362e0f49688743cdab
user <user@Server20>
2020-02-18 20:52:22 +0900

    firstCommit

---
+++ src/CompanyService.java
@@ -0,0 +1,22 @@
+
+public class CompanyService {
+
+	public void saveWorker() {
+		System.out.println("등록입니다");
+	}
+	
+	public void output(){
+		System.out.println("출력입니다");
+	}
+	
+	public void modify(){
+		
+		
+	}
+	public void search(){
+		
+		
+	}
+	
+	
+}
